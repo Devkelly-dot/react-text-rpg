c27201ad50b3853706ed18b98e7f4723438c23b2
2toesxtra <devkelly9622@gmail.com>
2023-07-04 10:27:45 -0400

    got rid of unused useEffect

--- src/components/text/dialog/dialogText.tsx
+++ src/components/text/dialog/dialogText.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import TypeWriter from "../../typewriter/typewriter";
 interface TextStyle {
     [key: string]: string;
